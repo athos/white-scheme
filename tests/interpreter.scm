266eb43f9f3f77795c5a71eb33ab673a1ca534b0
OHTA Shogo <athos0220@gmail.com>
2010-12-23 17:46:59 +0900

    implement parser

--- tests/interpreter.scm
+++ tests/interpreter.scm
@@ -10,6 +10,26 @@
 
 (test-section "test parser")
 
-(define main #f)
+(define (parse* code)
+  (with-module white-scheme.interpreter
+    (parse code *tokens->insn-parser*)))
+
+(define *code*
+  '(S S S T L L S S S T S S S S T T L S L S T L S T
+    S S S T S T S L T L S S S S S T L T S S S S L S
+    S S S T S T T L T S S T L T S S T S S S T S T L
+    L S L S T S S S S T T L L S S S T S S S T S T L
+    S L L L L L))
+
+(define *insns*
+  '((PUSH 1) (LABEL STSSSSTT) (DUP) (PUTN) (PUSH 10)
+    (PUTC) (PUSH 1) (ADD) (DUP) (PUSH 11) (SUB)
+    (BZERO STSSSTST) (JUMP STSSSSTT) (LABEL STSSSTST)
+    (POP) (QUIT)))
+
+(test* "parse code" *insns* (parse* *code*))
+(test* "incomplete instruction" (test-error) (parse* '(S T)))
+(test* "incomplete number" (test-error) (parse* '(S S S)))
+(test* "imcomplete label" (test-error) (parse* '(L S S S T)))
 
 (test-end)
